d43ab0da68a29364ff366ce63741b6e4a507e390
mrl-hzq <mrlhzq1216@gmail.com>
2024-05-28 21:03:05 +0800

    3rd commit

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,7 +4,7 @@ import 'leaflet/dist/leaflet.css';
 
 const Map = ({ pois }) => {
     return (
-    <MapContainer center={[3.1390, 101.6869]} zoom={13} style={{ height: '100vh', width: '100%' }}>
+    <MapContainer center={[3.1390, 101.6869]} zoom={13} style={{ height: '90vh', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
